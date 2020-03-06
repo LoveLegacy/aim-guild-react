43f3ee41ec45daf49ff466b66a384b889f456807
exori <confusingsivan@gmail.com>
2020-03-06 20:38:20 +0100

    Updates

---
+++ precache-manifest.24940c029c5f2e5afd302d9bab0fcfc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d66ef39400b2fc8d26fba09ee0e0b69b",
+    "url": "/aim-guild-react/index.html"
+  },
+  {
+    "revision": "99079be539c7a52131d6",
+    "url": "/aim-guild-react/static/js/2.62dfae03.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/aim-guild-react/static/js/2.62dfae03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc5d2203f2e55d134f0d",
+    "url": "/aim-guild-react/static/js/main.5079763f.chunk.js"
+  },
+  {
+    "revision": "1df8526bb54b2c3f852c",
+    "url": "/aim-guild-react/static/js/runtime-main.a178554f.js"
+  }
+]);
