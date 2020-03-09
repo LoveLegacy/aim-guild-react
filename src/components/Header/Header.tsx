dee944fbf01decc8f3d2f9b31f59ea0a51f61cf5
Gustav Sivander <gustavsivander@tele2.com>
2020-03-09 10:49:47 +0100

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div>
       <a href="https://www.sapperdem.com/api/v1/discord">Login</a>
+      <Link to={"/dashboard"}>Dashboard</Link>
     </div>
   );
 };
